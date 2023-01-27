8108c62384cc0ebc798c4ab64f5fdde09562ce96
Valentine Borisevich <valentineborisevich@gmail.com>
2023-01-27 12:02:37 +0300

    tasks 41 and 43 added

---
+++ task_041/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+
+
+Console.WriteLine("Enter array's length");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] Arr = new int[length];
+FillArray(Arr);
+Console.WriteLine("Your array is");
+Console.WriteLine(string.Join(", ", Arr));
