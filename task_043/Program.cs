d37b60e747ef2d60e790d022046c0a456608dbaa
Valentine Borisevich <valentineborisevich@gmail.com>
2023-01-27 12:59:30 +0300

    tasks 43 finished

--- task_043/Program.cs
+++ task_043/Program.cs
@@ -2,3 +2,19 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+Console.WriteLine("Please enter parameters of two lines(y = k1 * x + b1, y = k2 * x + b2;)");
+Console.WriteLine("And I will return it's intersection point coordinates");
+
+Console.WriteLine("Please enter b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1) / (-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"These lines' intersection point coordinates are: X: {x}, Y: {y}");
