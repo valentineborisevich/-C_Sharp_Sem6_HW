2612a7af0ce28eabead600a57c1a601704b2aac5
Valentine Borisevich <valentineborisevich@gmail.com>
2023-01-27 12:47:17 +0300

    tasks 41 finished

--- task_041/Program.cs
+++ task_041/Program.cs
@@ -4,11 +4,30 @@
 
 // 1, -7, 567, 89, 223-> 3
 
+void FillArrayManually(int number, int[] array)
+{
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write($"A[{i - 1 + 1}] = ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
 
 
 Console.WriteLine("Enter array's length");
 int length = Convert.ToInt32(Console.ReadLine());
-int[] Arr = new int[length];
-FillArray(Arr);
+int[] array = new int[length];
+FillArrayManually(length, array);
 Console.WriteLine("Your array is");
-Console.WriteLine(string.Join(", ", Arr));
+Console.WriteLine(string.Join(", ", array));
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("Number of digits bigger than 0 is: ");
+Console.WriteLine(count);
